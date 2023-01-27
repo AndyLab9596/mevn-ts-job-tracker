2cfb53380c7502fe1f0e727ad44b331cbcf12320
andyvy <thienvy95@gmail.com>
2023-01-27 15:25:07 +0700

    Update to App

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import 'module-alias/register';
-import validateEnv from './utils/validateEnv';
+import validateEnv from '@/utils/validateEnv';
 import App from "app";
+import UserController from '@/resources/user/user.controller';
 
 validateEnv();
 
-const app = new App([], Number(process.env.PORT));
+const app = new App([new UserController()], Number(process.env.PORT));
 app.listen();
