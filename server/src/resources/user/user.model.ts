49afc8ee6eb5dbbb3526a2f7b6d566ea47988fc2
vy.ct <vyct@mor.com.vn>
2023-01-31 17:33:23 +0700

    Inprogress Register

--- server/src/resources/user/user.model.ts
+++ server/src/resources/user/user.model.ts
@@ -20,10 +20,10 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: [true, 'Please provide email'],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email',
-        ],
+        // match: [
+        //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        //     'Please provide a valid email',
+        // ],
         unique: true,
     },
     password: {
